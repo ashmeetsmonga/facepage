0e30d3ca86ab6a9fb3ee63716b975b099129ff50
ashmeetsmonga <ashmeetsmonga@gmail.com>
2020-01-16 09:19:10 +0530

    Updates

---
+++ precache-manifest.5fced02ce9b02d1836c19412327beed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62aace10e3ba17d687f3d621509eaa2",
+    "url": "/facepage/index.html"
+  },
+  {
+    "revision": "4990e5f6a11f93d51eb4",
+    "url": "/facepage/static/css/main.9d5ea444.chunk.css"
+  },
+  {
+    "revision": "c2c81005ea688568b653",
+    "url": "/facepage/static/js/2.9e307b98.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facepage/static/js/2.9e307b98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4990e5f6a11f93d51eb4",
+    "url": "/facepage/static/js/main.fed95665.chunk.js"
+  },
+  {
+    "revision": "5d4e982b0226ffe642ea",
+    "url": "/facepage/static/js/runtime-main.0597a567.js"
+  }
+]);
